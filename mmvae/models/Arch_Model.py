9ed089ecd3a13f893c13b7bde32043c1bc5fe21a
hhong25 <hhong@eidexinsights.com>
2024-04-07 00:29:28 -0400

    m

--- mmvae/models/Arch_Model.py
+++ mmvae/models/Arch_Model.py
@@ -8,32 +8,32 @@ class VAE(nn.Module):
         super(VAE, self).__init__()
         #Encoder
         self.encoder = nn.Sequential(
-            nn.Linear(60664, 1024),
+            nn.Linear(60664, 4096),
             nn.ReLU(),
-            nn.BatchNorm1d(1024, 0.8),
-            nn.Linear(1024, 768),
+            nn.BatchNorm1d(4096, 0.8),
+            nn.Linear(4096, 1200),
             nn.ReLU(),
-            nn.BatchNorm1d(768, 0.8),
-            nn.Linear(768, 256),
+            nn.BatchNorm1d(1200, 0.8),
+            nn.Linear(1200, 400),
             nn.ReLU(),
-            nn.BatchNorm1d(256, 0.8)
+            nn.BatchNorm1d(400, 0.8)
         )
         
-        self.fc_mu = nn.Linear(256, 128)
-        self.fc_var = nn.Linear(256, 128)
+        self.fc_mu = nn.Linear(400, 128)
+        self.fc_var = nn.Linear(400, 128)
         
         # Decoder
         self.decoder = nn.Sequential(
-            nn.Linear(128, 256),
+            nn.Linear(128, 400),
             nn.ReLU(),
-            nn.BatchNorm1d(256, 0.8),
-            nn.Linear(256, 768),
+            nn.BatchNorm1d(400, 0.8),
+            nn.Linear(400, 1200),
             nn.ReLU(),
-            nn.BatchNorm1d(768, 0.8),
-            nn.Linear(768, 1024),
+            nn.BatchNorm1d(1200, 0.8),
+            nn.Linear(1200, 4096),
             nn.ReLU(),
-            nn.BatchNorm1d(1024, 0.8),
-            nn.Linear(1024, 60664),
+            nn.BatchNorm1d(4096, 0.8),
+            nn.Linear(4096, 60664),
             nn.Sigmoid(),
         )
 
